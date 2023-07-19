8378604c4ca72da4639d52bd3e1a6f01c4830944
suttala <alarik.sutter@iceye.fi>
2023-07-19 11:13:56 +0300

    Defined addresses, masks, and ADC Modes

--- ChipControl.h
+++ ChipControl.h
@@ -3,17 +3,33 @@
 
 #include "LTC2943.h"
 
+/* Defining register addresses */
+#define LTC2943_A_STATUS      0b00000000
+#define LTC2943_B_CONTROL     0b00000001
+#define LTC2943_B_CONTROL     0b00000001
+#define LTC2948_E_CHRG_TRSHLD 0b00000100
+#define LTC2948_F_CHRG_TRSHLD 0b00000101
+#define LTC2948_G_CHRG_TRSHLD 0b00000110
+#define LTC2948_H_CHRG_TRSHLD 0b00000111
+
+/* Defining operation masks */
+#define ADC_MODE_MASK         0b11000000
+#define TEMP_ALRT_MASK        0b00010000
+#define CHRG_ALRT_HI_MASK     0b00001000
+#define CHRG_ALRT_LO_MASK     0b00000100
+
+/* ADC modes */
+#define ADC_MODE_AUTOMATIC    0b11000000
+#define ADC_MODE_SCAN         0b10000000
+#define ADC_MODE_MANUAL       0b01000000
+#define ADC_MODE_SLEEP        0b00000000
+
+/* Command status codes */
 typedef enum {
     CMD_STATUS_SUCCESS = 0,
     CMD_STATUS_FAILURE = 1,
 } cmd_status_t;
 
-typedef enum {
-    ADC_MODE_AUTO = 0,
-    ADC_MODE_SCAN = 1,
-    ADC_MODE_MANUAL = 2,
-    ADC_MODE_SLEEP = 3,
-} adc_mode_t;
 
 cmd_status_t get_adc_mode();
 
