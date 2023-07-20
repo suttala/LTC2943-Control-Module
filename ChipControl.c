89a3aa139c2fa2a0cd93a8b2b2a2ac2ba745c729
suttala <alarik.sutter@iceye.fi>
2023-07-20 10:42:05 +0300

    Specified Error types in Error codes & Typo fixes

--- ChipControl.c
+++ ChipControl.c
@@ -19,13 +19,13 @@ static uint8_t get_low_byte(uint16_t data) {
 }
 
 
-/* LTC2948 CHIPCONTROL API */
+/* LTC2943 CHIPCONTROL API */
 
 
 cmd_status_t get_adc_mode(adc_mode_t *resp) {
     uint8_t data;
     if (!LTC2943_Read(LTC2943_B_CONTROL, &data, 1)) {
-        return CMD_STATUS_FAILURE;
+        return CMD_STATUS_READ_FAILURE;
     }
     *resp = get_bits_by_mask(data, MASK_ADC_MODE);
     return CMD_STATUS_SUCCESS;
@@ -34,20 +34,24 @@ cmd_status_t get_adc_mode(adc_mode_t *resp) {
 cmd_status_t set_adc_mode(adc_mode_t adc_mode) {
     uint8_t data;
     if (!LTC2943_Read(LTC2943_B_CONTROL, &data, 1)) {
-        return CMD_STATUS_FAILURE;
+        return CMD_STATUS_READ_FAILURE;
     }
     if (get_bits_by_mask(data, MASK_ADC_MODE) == adc_mode) {
         return CMD_STATUS_SUCCESS;
     }
 
     set_bits_by_mask(&data, MASK_ADC_MODE, adc_mode);
-    return (cmd_status_t)LTC2943_Write(LTC2943_B_CONTROL, &data, 1);
+
+    if (!LTC2943_Write(LTC2943_B_CONTROL, &data, 1)) {
+        return CMD_STATUS_WRITE_FAILURE;
+    }
+    return CMD_STATUS_SUCCESS;
 }
 
 cmd_status_t check_temp_alert(temp_status_t *resp) {
     uint8_t data;
     if (!LTC2943_Read(LTC2943_A_STATUS, &data, 1)) {
-        return CMD_STATUS_FAILURE;
+        return CMD_STATUS_READ_FAILURE;
     }
 
     *resp = get_bits_by_mask(data, MASK_TEMP_ALRT);
@@ -57,7 +61,7 @@ cmd_status_t check_temp_alert(temp_status_t *resp) {
 cmd_status_t check_charge_status(chrg_status_t *resp) {
     uint8_t data;
     if (!LTC2943_Read(LTC2943_A_STATUS, &data, 1)) {
-        return CMD_STATUS_FAILURE;
+        return CMD_STATUS_READ_FAILURE;
     }
 
     *resp = get_bits_by_mask(data, MASK_CHRG_ALRT);
@@ -71,16 +75,16 @@ cmd_status_t set_charge_treshold(uint16_t min, uint16_t max) {
     uint8_t min_lsb = get_low_byte(min);
 
     if (!LTC2943_Write(LTC2943_E_CHRG_TRSHLD, &max_msb, 1)) {
-        return CMD_STATUS_FAILURE;
+        return CMD_STATUS_WRITE_FAILURE;
     }
     if (!LTC2943_Write(LTC2943_F_CHRG_TRSHLD, &max_lsb, 1)) {
-        return CMD_STATUS_FAILURE;
+        return CMD_STATUS_WRITE_FAILURE;
     }
     if (!LTC2943_Write(LTC2943_G_CHRG_TRSHLD, &min_msb, 1)) {
-        return CMD_STATUS_FAILURE;
+        return CMD_STATUS_WRITE_FAILURE;
     }
     if (!LTC2943_Write(LTC2943_H_CHRG_TRSHLD, &min_lsb, 1)) {
-        return CMD_STATUS_FAILURE;
+        return CMD_STATUS_WRITE_FAILURE;
     }
     return CMD_STATUS_SUCCESS;
 }
--- ChipControl.h
+++ ChipControl.h
@@ -19,38 +19,39 @@
 
 /* Command status codes */
 typedef enum {
-    CMD_STATUS_FAILURE = 0, // Sending command was unsuccessful
-    CMD_STATUS_SUCCESS = 1, // Sending command was successful
+    CMD_STATUS_SUCCESS       = 0, // Executing command was successful
+    CMD_STATUS_READ_FAILURE  = 1, // Error in reading value from the LTC2943
+    CMD_STATUS_WRITE_FAILURE = 2, // Error in writing value to the LTC2943
 } cmd_status_t;
 
 /* ADC modes */
 typedef enum {
-    LTC2948_ADC_MODE_AUTO   = 0xC0,
-    LTC2948_ADC_MODE_SCAN   = 0x80,
-    LTC2948_ADC_MODE_MANUAL = 0x40,
-    LTC2948_ADC_MODE_SLEEP  = 0x00,
+    LTC2943_ADC_MODE_AUTO   = 0xC0,
+    LTC2943_ADC_MODE_SCAN   = 0x80,
+    LTC2943_ADC_MODE_MANUAL = 0x40,
+    LTC2943_ADC_MODE_SLEEP  = 0x00,
 } adc_mode_t;
 
 /* Charge status codes */
 typedef enum {
-    LTC2948_CHRG_STATUS_OK    = 0x00, // Charge is between tresholds
-    LTC2948_CHRG_STATUS_HIGH  = 0x08, // Charge is above the high treshold
-    LTC2948_CHRG_STATUS_LOW   = 0x04, // Charge is below the low treshold
-    LTC2948_CHRG_STATUS_ERROR = 0x0C, // Both HIGH and LOW statuses reported
+    LTC2943_CHRG_STATUS_OK    = 0x00, // Charge is between tresholds
+    LTC2943_CHRG_STATUS_HIGH  = 0x08, // Charge is above the high treshold
+    LTC2943_CHRG_STATUS_LOW   = 0x04, // Charge is below the low treshold
+    LTC2943_CHRG_STATUS_ERROR = 0x0C, // Both HIGH and LOW statuses reported
 } chrg_status_t;
 
 /* Temperature status codes */
 typedef enum {
-    LTC2948_TEMP_STATUS_OK    = 0x00, // No temperature alert
-    LTC2948_TEMP_STATUS_ALERT = 0x10, // Temperature alert found
+    LTC2943_TEMP_STATUS_OK    = 0x00, // No temperature alert
+    LTC2943_TEMP_STATUS_ALERT = 0x10, // Temperature alert found
 } temp_status_t;
 
 
-/* LTC2948 CHIPCONTROL API */
+/* LTC2943 CHIPCONTROL API */
 
 
 /**
- * @brief Get the current LTC2948 ADC mode
+ * @brief Get the current LTC2943 ADC mode
  *
  * @param resp : uint8_t where the two MSB represent the ADC mode
  * @return cmd_status_t
@@ -58,7 +59,7 @@ typedef enum {
 cmd_status_t get_adc_mode(adc_mode_t *resp);
 
 /**
- * @brief Set the LTC2948 ADC mode
+ * @brief Set the LTC2943 ADC mode
  *
  * @param adc_mode : New ADC to be set
  * @return cmd_status_t
@@ -66,7 +67,7 @@ cmd_status_t get_adc_mode(adc_mode_t *resp);
 cmd_status_t set_adc_mode(adc_mode_t adc_mode);
 
 /**
- * @brief Checks the LTC2948 temperature alert status
+ * @brief Checks the LTC2943 temperature alert status
  *
  * @param resp : temp_status_t value where 0 is OK and 1 an active alert
  * @return cmd_status_t
@@ -74,7 +75,7 @@ cmd_status_t set_adc_mode(adc_mode_t adc_mode);
 cmd_status_t check_temp_alert(temp_status_t *resp);
 
 /**
- * @brief Set LTC2948 charge tresholds
+ * @brief Set LTC2943 charge tresholds
  *
  * @param min : Minimum charge treshold to be set
  * @param max : Maximum charge treshold to be set
@@ -83,7 +84,7 @@ cmd_status_t check_temp_alert(temp_status_t *resp);
 cmd_status_t set_charge_treshold(uint16_t min, uint16_t max);
 
 /**
- * @brief Check if the LTC2948 charge is within its tresholds
+ * @brief Check if the LTC2943 charge is within its tresholds
  *
  * @param resp : chrg_status_t value
  * @return cmd_status_t
